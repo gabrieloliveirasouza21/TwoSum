0f3bc219d2658136ba0fcb88b17b2524395df632
gabrieloliveirasouza21 <gabrielosouza21@gmail.com>
2023-10-22 12:18:46 -0300

    Implementar sem itens adjacentes

--- Program.cs
+++ Program.cs
@@ -1,22 +1,21 @@
 ﻿internal class Program {
     private static void Main(string[] args) {
-        int[] arr = {6,7,4,2};
-        TwoSum(arr, 6);
+        int[] arr = {6};
+        Console.WriteLine(TwoSum(arr, 6));
+        
         
 
     }
 
-    public static void TwoSum(int[] nums, int target) {
+    public static int[] TwoSum(int[] nums, int target) {
 
-        if (nums.Length < 2) {
-            Console.WriteLine("menor que 2 elementos");
-        }
+        
 
         Dictionary<string,int> sums = new Dictionary<string,int>();
 
         for (int i = 0; i < nums.Length - 1; i++) {
             int contador = i + 1;
-            sums.Add(i.ToString() + " "+ contador.ToString(), nums[i] + nums[i+1]);
+            sums.Add(i.ToString() + " "+ contador.ToString(), nums[i] + nums[contador]);
             
         }
 
@@ -30,11 +29,12 @@
             if (s.Value.Equals(target)) {
 
                 char[] chaves = s.Key.ToCharArray();
-                //Console.WriteLine("chaves[0] " + chaves[0]);
-                //Console.WriteLine("chaves[1] " + chaves[2]);
+                Console.WriteLine("chaves[0] " + chaves[0]);
+                Console.WriteLine("chaves[1] " + chaves[2]);
                 arrFinal[0] = chaves[0] - 48;
                 arrFinal[1] = chaves[2] - 48;
                 break;
+               
             }
         }
 
@@ -42,6 +42,6 @@
             Console.WriteLine(s);
         }
 
-        //return arrFinal;
+        return arrFinal;
     }
 }
