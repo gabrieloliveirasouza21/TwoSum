e10b69db808afb58c0b2be28e85c09af8bcc06e1
gabrieloliveirasouza21 <gabrielosouza21@gmail.com>
2023-10-22 21:51:10 -0300

    O(n) implementado

--- Program.cs
+++ Program.cs
@@ -15,20 +15,45 @@
 
     public static int[] TwoSum(int[] nums, int target) {
 
+        //O código a seguir é de complexidade O(n^2)
+
+        //for (int i = 0; i < nums.Length; i++) {
+        //    int atual = nums[i];
+        //    for (int j = 0; j<nums.Length; j++) {
+        //        if (j.Equals(i)) {
+        //            continue;
+        //        }
+        //        if (atual + nums[j] == target) {
+        //            return new int[] { i, j };
+        //        }
+        //    }
+        //}
+
+        //return new int[0];
+
+        //---------------------------------------------------------
+
+        //O código a seguir é de complexidade O(n)
+
+        Dictionary<int, int> dic = new Dictionary<int, int>();
+
         for (int i = 0; i < nums.Length; i++) {
-            int atual = nums[i];
-            for (int j = 0; j<nums.Length; j++) {
-                if (j.Equals(i)) {
-                    continue;
-                }
-                if (atual + nums[j] == target) {
-                    return new int[] { i, j };
-                }
+
+            int falta =  target - nums[i];
+
+            if (dic.ContainsKey(falta)){
+                return new int[] { dic[falta], i };
             }
+
+            dic[nums[i]] = i; 
+
         }
-
         return new int[0];
-
-
+        
+        //Esse código cria um hash map, percorre o array
+        // e guarda a diferença entre o numero alvo e cada item do array
+        // se tem esse numero dessa diferença no hash map, significa que um dos numeros
+        // do array é essa diferença
+        // então ele retorna um novo array com a posição desses membros
     }
 }
